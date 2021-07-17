3c228c84627b94a195f3e86f6b03c38722894779
jurgen1010 <jurgen1010@hotmail.com>
2021-07-17 07:14:34 -0500

    Navegar a clientes y productos

---
+++ POSUNO/POSUNO/POSUNO.Shared/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POSUNO.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; }
+
+        
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public float Stock { get; set; }
+
+        public bool IsActive { get; set; }
+        //Un producto pertenece a un usuario y usuario puede tener muchos productos (1 a muchos)
+        
+        public User User { get; set; }
+    }
+}
--- POSUNO/POSUNO/POSUNO.Shared/Pages/MainPage.xaml.cs
+++ POSUNO/POSUNO/POSUNO.Shared/Pages/MainPage.xaml.cs
@@ -32,8 +32,10 @@ namespace POSUNO.Pages
         {
             base.OnNavigatedTo(e);
             User = (User)e.Parameter;
-
             WelcomeTextBlock.Text = $"Bienvenid@: {User.FullName}";
+            
+            //Cargar por defecto en la pagina de clientes
+            MyFrame.Navigate(typeof(CustomersPage));
         }
 
         private async void LogoutImage_Tapped(object sender, TappedRoutedEventArgs e)
@@ -56,6 +58,15 @@ namespace POSUNO.Pages
             };
 
             return await confirmDialog.ShowAsync();
+        }
+
+        private void CustomersNavigationViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(CustomersPage));
+
+        }private void ProductsNavigationViewItem_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            MyFrame.Navigate(typeof(ProductsPage));
         }
     }
 }
